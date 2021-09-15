6eef7efc0804c1e1fae7df61e4455ab71caafb9e
Bin <libin.im@gmail.com>
2021-09-14 22:51:55 -0700

    Update reader.go

--- flv/reader.go
+++ flv/reader.go
@@ -18,6 +18,11 @@ func NewReader(r io.Reader) *Reader {
 }
 
 // ReadHeader reads FLV header
+/*
+FLV文件头由9bytes组成，前3个bytes是文件类型，总是“FLV”，也就是（0x46 0x4C 0x56）。第4btye是版本号，目前一般是0x01。
+第5byte是流的信息，倒数第一bit是1表示有视频（0x01），倒数第三bit是1表示有音频（0x4），有视频又有音频就是0x01 | 0x04（0x05），其他都应该是0。
+最后4bytes表示FLV 头的长度，3+1+1+4 = 9。
+*/
 func (r *Reader) ReadHeader() (*Header, error) {
 	b, err := r.next(9)
 	if err != nil {
@@ -35,6 +40,22 @@ func (r *Reader) ReadHeader() (*Header, error) {
 
 // ReadTag reads FLV tag and returns payload reader.
 // Reader is not valid after next ReadTag.
+/*
+FLV body由若干个tag 组成。每一个tag第一部分是tag header，tag header长度为11bytes，但是每个tag header前面有4bytes记录着上一个tag的长度。
+tag header：
+
+        １）第1个byte为记录着tag的类型，音频（0x8），视频（0x9），脚本（0x12）；
+
+        ２）第2到4bytes是数据区的长度，也就是tag data的长度；
+
+        ３）再后面3个bytes是时间戳，单位是毫秒，类型为0x12则时间戳为0，时间戳控制着文件播放的速度，可以根据音视频的帧率类设置；
+
+        ４）时间戳后面一个byte是扩展时间戳，时间戳不够长的时候用；
+
+        ５）最后3bytes是streamID，但是总为0，再后面就是数据区了（tag data），也即是h264的裸流；
+
+        ６）tag header 长度为1+3+3+1+3=11。
+*/
 func (r *Reader) ReadTag() (*Tag, io.Reader, error) {
 	b, err := r.next(15)
 	if err != nil {
